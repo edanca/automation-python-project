6eee07a663f68e289dd72eb55a414913cf1124d9
Eduardo Cardenas <Eduardo_Cardenas@bose.com>
2020-01-26 01:26:24 -0300

    Including docker and docker-compose compatibility

--- test_fmk/conftest.py
+++ test_fmk/conftest.py
@@ -1,15 +1,23 @@
+from os import environ
 from pytest import fixture
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
+from time import sleep
+
+env = environ.get('MY_ENV', 'local')
 
 
 @fixture(scope='module')
 def browser():
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('--incognito')
-    # browser = webdriver.Chrome(options=options)
-    # url_selenium = 'http://chrome:4444/wd/hub'
-    browser = webdriver.Remote(desired_capabilities=DesiredCapabilities.CHROME)
+    browser = None
+    if env == 'local':
+        options = webdriver.ChromeOptions()
+        options.add_argument('--incognito')
+        browser = webdriver.Chrome(options=options)
+    elif env == 'docker':
+        sleep(3)
+        url_selenium = 'http://chrome:4444/wd/hub'
+        browser = webdriver.Remote(url_selenium, desired_capabilities=DesiredCapabilities.CHROME)
 
     yield browser
 
